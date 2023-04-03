f47f29b3aed6dbd762d9399a789c5cac2c4f0bcc
Pavel-b-p <pavelbakin38@gmail.com>
2023-04-03 21:10:58 +0300

    Внёс правки и добавил подключение
    Подключение в App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 import axios from 'axios';
 
-const src = "";
+const src = "http://93.187.181.236:26556";
 
 function App() {
 
-  axios.get(src).then(data => {
+  axios.get(src).then(data => 
+  {
     console.log(data);
   });
 
   return (
     <div className="App">
-      
+      ПРИВЕТ, КОТЯТА
     </div>
   );
 }
