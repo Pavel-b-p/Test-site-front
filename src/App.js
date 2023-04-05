f32e250df42c703a71ed2fc935b029ed9711dfbd
Pavel-b-p <pavelbakin38@gmail.com>
2023-04-05 12:28:42 +0300

    Добавлены папки

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const src = "http://93.187.181.236:26556";
 
-function App() {
+function App() 
+{
 
   axios.get(src).then(data => 
   {
@@ -12,6 +13,9 @@ function App() {
   return (
     <div className="App">
       ПРИВЕТ, КОТЯТА
+      <div>
+        <img src="https://avatars.mds.yandex.net/i?id=3244ea7879b162250059300b984e87bf-5221713-images-thumbs&n=13"/>
+      </div>
     </div>
   );
 }
